a17d9b860179183eb657c9ff4f3c421f5fc40f64
Евгений Бузовский <zhenyabuzovskii@gmail.com>
2022-01-26 23:08:48 +0300

    localization

--- src/main/ng/frontend/src/app/modules/bars/footer-block/footer-block/footer-block.component.ts
+++ src/main/ng/frontend/src/app/modules/bars/footer-block/footer-block/footer-block.component.ts
@@ -1,6 +1,7 @@
 import {ChangeDetectionStrategy, EventEmitter, Component, Input, Output, OnInit} from '@angular/core';
 import {MatSlideToggleChange} from "@angular/material/slide-toggle";
 import {TranslateService} from "@ngx-translate/core";
+import { AuthService } from 'src/app/service/auth/auth.service';
 
 interface Lang {
   viewValue: string;
@@ -17,16 +18,18 @@ export class FooterBlockComponent implements OnInit{
 
   selectedValue: string;
 
-  constructor(public translate:TranslateService) {}
+  constructor(public translate:TranslateService,private auth:AuthService) {}
 
   ngOnInit(): void {
     this.translate.addLangs(['en','ru']);
     this.translate.setDefaultLang('en');
-    if (localStorage.getItem('locale')){
-      const browserLang = localStorage.getItem('locale');
-      this.translate.use(browserLang.match(/en|ru/)? browserLang : 'en');
-    }else {
-      localStorage.setItem('locale','en');
+    if (this.auth.user!=null){
+      if (localStorage.getItem('locale')){
+        const browserLang = localStorage.getItem('locale');
+        this.translate.use(browserLang.match(/en|ru/)? browserLang : 'en');
+      }else {
+        localStorage.setItem('locale','en');
+      }
     }
   }
 
